6f2bc9e1527fbe73a68ff3cec8a548c982119d31
Amit Shmulevitch <amitsh1995@gmail.com>
2023-08-04 14:08:39 +0300

    bump version 0.3.2 -> 0.3.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ import sys
 project = 'sylk'
 copyright = 'sylk 2022, Amit Shmulevitch'
 author = 'Amit Shmulevitch'
-release = '0.3.2'
+release = '0.3.3'
 
 sys.path.append('/Users/amitshmulevitch/Projects/sylk/sylk')
 # -- General configuration ---------------------------------------------------
